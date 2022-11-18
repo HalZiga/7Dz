58b8fd24e5acbb8ed566223096a3cf208ba89686
HalZiga <113126146+HalZiga@users.noreply.github.com>
2022-11-18 13:38:28 +0300

    Добавьте файлы проекта.

---
+++ 7Dz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Dz
+{
+    public enum Tip
+    {
+        Tekushiy = 1,
+        Sberegatelniy = 2
+    }
+    
+
+    internal class Program
+    {
+        public static void Zad82()
+        {
+            Console.WriteLine("Введите строку");
+            string a = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                sb.Append(a[i]);
+            }
+            Console.WriteLine(sb);
+        }
+
+        static void Main(string[] args)
+        {
+            Schet schet1 = new Schet(777,666,Tip.Tekushiy);
+            Schet schet2 = new Schet(234, 696, Tip.Sberegatelniy);
+
+            Console.WriteLine("Какую сумму хотите перевести из первого банка во второй?");
+            
+            
+            int monney = Convert.ToInt32(Console.ReadLine());
+            schet1.moneytransfer(schet2, monney);
+            schet1.Print();
+            schet2.Print();
+
+            //Console.WriteLine("Задание 8.2");
+            //Zad82();
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ 7Dz/tip/Schet.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Dz
+{
+    public class Schet
+    {
+        private static uint nomer;
+        private float balanse;
+        private Tip tip;
+        private static HashSet<uint> Last_Random = new HashSet<uint> { };
+        public Schet(uint _nomer, float _balanse, Tip _trip)
+        {
+            nomer = _nomer;
+            while (!Last_Random.Add(nomer))//закидываем в HashSet и проверяем был или нет
+            {
+                nomer += 1;
+            }
+            balanse = _balanse;
+            tip = _trip;
+            Print();
+        }
+        public Schet()
+        {
+            nomer++;
+            while (!Last_Random.Add(nomer))//закидываем в HashSet и проверяем был или нет
+            {
+                nomer += 1;
+            }
+            balanse = Rand();
+            tip = (Tip)1;
+
+        }
+        public uint Rand()
+        {
+            Random rnd = new Random();
+            uint b = (uint)rnd.Next(0, 99999);//как сделать так чтобы метод в котором рандом при разных вызовах генерировал разные значения
+            return b;
+        }
+
+
+
+        public bool Snat(float temp)
+        {
+            //Console.Write("Введите сумму для пополнения ");
+            //float temp = float.Parse(Console.ReadLine());
+            if (balanse - temp >= 0)
+            {
+                return true;
+                //Console.WriteLine($"Балансе на данный момент: {balanse -= temp}");
+            }
+            else
+            { 
+                //Console.WriteLine("Вы не можете снять такую сумму");
+                return false;
+            }
+        }
+        public void Popoln()
+        {
+            Console.Write("Введите сумму для пополнения ");
+            float temp = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Балансе на данный момент: {balanse += temp}");
+
+        }
+        public void Print()
+        {
+            Console.WriteLine("Номер: " + nomer);
+            Console.WriteLine("Баланс: " + balanse);
+            Console.WriteLine("Тип: " + tip);
+        }
+        public void moneytransfer(Schet Bank, int amount)
+        {
+            
+            if (Snat(amount))
+            {
+                this.balanse -= amount;
+                Bank.balanse += amount;
+            }
+            else
+            {
+                Console.WriteLine("Вы не можете снять такую сумму");
+            }
+            
+        }
+    }
+}
